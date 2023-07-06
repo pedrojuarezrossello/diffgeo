7b86a36219adeb715d93b5ee7cf91c894c7f97e1
pedrojuarezrossello <pedrojuarez33@gmail.com>
2023-07-06 21:22:39 +0100

    Vector Field

--- src/forms/two_form_interface.h
+++ src/forms/two_form_interface.h
@@ -1,6 +1,7 @@
 #ifndef TWO_FORM_INTERFACE_H
 #define TWO_FORM_INTERFACE_H
 #include "../utils/real_function.h"
+#include "../forms/vector_field.h"
 
 namespace dg {
 
@@ -23,6 +24,14 @@ namespace dg {
 
 			TwoFormInterface(std::function<T(T, T)> A, std::function<T(T, T)> B, std::function<T(T, T)> C)
 				: A_(A), B_(B), C_(C) {}
+
+			template<typename U>
+			auto innerProduct(const VectorField<U>& vector1, const VectorField<U>& vector2, U var1, U var2)
+			{
+				return A_(var1, var2) * vector1.compute_U(var1, var2) * vector2.compute_U(var1, var2)
+					+ B_(var1, var2) * (vector1.compute_U(var1, var2) * vector2.compute_V(var1, var2) + vector1.compute_V(var1, var2) * vector2.compute_U(var1, var2))
+					+ C_(var1, var2) * vector1.compute_V(var1, var2) * vector2.compute_V(var1, var2);
+			}
 			
 		};
 
